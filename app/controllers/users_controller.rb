38cdd273b09c37b510fd1ec28a4a8287ad7e062a
takasugiren <bucyou16528080@gmail.com>
2021-07-12 11:24:25 +0000

    ユーザー詳細、編集　レイアウト修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:update, :edit]
+
   def show
     @user = User.find(params[:id])
+    @tweets = @user.tweets.all
   end
 
   def edit
@@ -14,7 +18,15 @@ class UsersController < ApplicationController
   end
 
   private
+
   def user_params
     params.require(:user).permit(:name, :profile_image, :profile, :ratio, :rank)
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
   end
 end
