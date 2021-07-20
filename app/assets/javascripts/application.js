4f246af25b746bcc17663b449b2cec03d5e8d028
takasugiren <bucyou16528080@gmail.com>
2021-07-20 14:21:08 +0000

    検索結果画面　追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,10 +28,11 @@ $(function() {
   });
 });
 
-$(function(){
+$(document).on('turbolinks:load', function() {
   $('.menu-trigger').on('click', function(event) {
     $(this).toggleClass('active');
     $('#sp-menu').fadeToggle();
     event.preventDefault();
   });
 });
+
