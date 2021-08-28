3e17a4d8c2066a63535a230db1617f437fc3d53e
takasugiren <bucyou16528080@gmail.com>
2021-08-28 14:10:13 +0000

    [Update]DM機能

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -6,8 +6,8 @@ class ChatsController < ApplicationController
     # user_roomモデルからuser_idがチャット相手のidが一致するものと
     # room_idが上記roomsのどれかに一致するレコードをuser＿roomsに代入
     user_rooms = UserRoom.find_by(user_id: @user.id, room_id: rooms)
-    
-    # もしuser＿roomが空でないなら 
+
+    # もしuser＿roomが空でないなら
     unless user_rooms.nil?
       @room = user_rooms.room
     else
@@ -17,16 +17,16 @@ class ChatsController < ApplicationController
       UserRoom.create(user_id: current_user.id, room_id: @room.id)
       UserRoom.create(user_id: @user.id, room_id: @room.id)
     end
-    @chats = @rom.chats
+    @chats = @room.chats
     @chat = Chat.new(room_id: @room.id)
   end
-  
+
   def create
     @chat = current_user.chats.new(chat_params)
     @chat.save
     redirect_to request.referer
   end
-  
+
   private
   def chat_params
     params.require(:chat).permit(:message, :room_id)
